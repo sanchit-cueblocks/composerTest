67eac19582ac70251918bbf6dab34c38afdc0de1
Sanchit <sanchit@cueblocks.com>
2019-12-13 12:51:51 +0530

    third commit

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'ComposerTestModule_TestModule',
+    'Cmworking_TestModule',
     __DIR__
 );
